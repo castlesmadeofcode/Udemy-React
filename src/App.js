c5c5fb0bfbfa843cf064022a455d5f52e94ff4a7
castle <castlesmadeofcode@gmail.com>
2020-07-27 13:34:00 -0500

    understanding JSX

--- src/App.js
+++ src/App.js
@@ -3,19 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  // creates a new class object that inherits from Component class which is imported from the react library
+  // Components are basically custom HTML elements
   render() {
+    // React calls render to      | return/render something to the screen/DOM
+    // every react component must ^
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Hi, I'm a React App</h1>
       </div>
     );
+    // JSX not hmtl^
+    // gets compiled to:
+    // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'does this work now?'))
   }
 }
 
+
 export default App;
+// default export / if you import this whole file you import the (App) class
