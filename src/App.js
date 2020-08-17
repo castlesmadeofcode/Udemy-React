bfd88870fc0fa45a5bf1622f3717d582e7aebad0
castle <castlesmadeofcode@gmail.com>
2020-08-17 11:50:36 -0500

    Section 4: Working with Lists and Conditionals -complete

--- src/App.js
+++ src/App.js
@@ -12,43 +12,49 @@ class App extends Component {
   // if anything in state changes react will rerender/update the dom
   state = {
     persons: [
-      { name: 'Max', age: 28 },
-      { name: 'Manu', age: 29 },
-      { name: 'Stephanie', age: 26 }
+      { id: 'a', name: 'Max', age: 28 },
+      { id: 'b', name: 'Manu', age: 29 },
+      { id: 'c', name: 'Stephanie', age: 26 }
     ],
     showPersons: false
   }
 
   // Handler is used to indicate this is a method you arnet actively calling, but assigning as an event handler
-  switchNameHandler = (newName) => {
-    // console.log('Was clicked!')
-    // DONT DO THIS this.state.persons[0].name = 'Maximilian'
-    this.setState({
-      persons: [
-        { name: newName, age: 28 },
-        { name: 'Manu', age: 29 },
-        { name: 'Stephanie', age: 27 }
-      ]
-    })
+  deletePersonHandler = (personIndex) => {
+    // [...] spread operator creates copy of an array
+    // always update state in an immutable fashion, create a copy change that than update the state
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons })
+
     // the component object has a setState method which allows us to update the state property which updates the DOM
     // set state takes an object as an argument and will merge whatever we define within it with our existing state
   }
 
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: 'Max', age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: 'Stephanie', age: 26 }
-      ]
-    })
+  nameChangedHandler = (event, id) => {
+    // find
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({ persons: persons })
   }
 
 
   // we use an arrow function so 'this' always returns to this class
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
+    // if doeShow is true set showPersons to false is doesShow is true set showPersons to false
     this.setState({ showPersons: !doesShow })
 
   }
@@ -66,6 +72,35 @@ class App extends Component {
       cursor: 'pointer'
     };
 
+    let persons = null;
+
+    if (this.state.showPersons) {
+
+      // convert array of objects into valid jsx
+      //function is executed on each element in the persons array and returns a person component
+      persons = (
+        < div >
+          {/* map method exposes a second argument, index. */}
+          {/* execute as an arrow function in order to pass index */}
+          {this.state.persons.map((person, index) => {
+            return <Person
+              click={() => this.deletePersonHandler(index)}
+              name={person.name}
+              age={person.age}
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id)} />
+
+            {/* we assign a key property to allow react to keep track of the individual elements 
+         so that it has a clear property it can compare between the different elements to
+         found out which element changed and which didnt so that it only rerenders the elements that 
+         did change and not the whole list */}
+
+          })}
+        </div >
+
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
@@ -75,23 +110,7 @@ class App extends Component {
           onClick={this.togglePersonsHandler}>Switch Name</button>
         {/* /* this refers to the class  */}
 
-        {/* if showPersons = true display the div else render null/nothing */}
-        {
-          this.state.showPersons ?
-            < div >
-              <Person name={this.state.persons[0].name}
-                age={this.state.persons[0].age}></Person>
-              <Person name={this.state.persons[1].name}
-                age={this.state.persons[1].age}
-                // bind (this) and list of arguments that get passed into our function (switchNameHandler(newName))
-                click={this.switchNameHandler.bind(this, 'Max')}
-                // add a new property (click) and pass reference to the function (this.switchNameHandler) you want to pass as props
-                changed={this.nameChangedHandler}>My Hobbies: Racing</Person>
-              <Person name={this.state.persons[2].name}
-                age={this.state.persons[2].age}></Person>
-            </div >
-            : null
-        }
+        {persons}
       </div>
     );
     // JSX not hmtl^
